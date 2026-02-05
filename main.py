f125f2a7cc5d680c37b0c5b75a4ad03db1f1f000
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2026-02-05 12:05:43 +0100

    feat: implement Player model

--- main.py
+++ main.py
@@ -20,6 +20,22 @@ def load_game_data(filepath: str) -> dict:
         
     except Exception as e:
         raise RuntimeError(f"Erreur inattendue : {e}")
+
+
+class Player:
+    """
+    Represente le joueur explorateur et son état courant dans la partie.
+    """
+    
+    def __init__(self, name: str, health_points: int=20):
+        """
+        Initialise un joueur avec ces caractéristiques de départ.
+        """
+        self.name = name
+        self.health_points = health_points
+        self.force = random.randint(1, 10)
+        self.inventory = {}
+    
     
 def main():
     pass
